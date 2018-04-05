2dfb64f7666bfc5af8628350f0f75c2e5463c09a
zdcthomas <zdcthomas@yahoo.com>
2018-04-05 16:54:11 -0600

    clean up

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,19 @@
 require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  def test_it_exists
+    reunion = Reunion.new("Denver")
+    assert_instance_of Reunion, reunion
+  end 
+
+  def test_reunion_has_location
+    reunion = Reunion.new("Denver")
+    assert_equal "Denver", reunion.location
+  end 
+  
 end
