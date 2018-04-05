c45e34b65e213a986d4a3693fe80432105908165
zdcthomas <zdcthomas@yahoo.com>
2018-04-05 17:19:03 -0600

    TESTS breakdown on iteration 4

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -30,4 +30,34 @@ class ReunionTest < Minitest::Test
     assert_equal [hiking], reunion.activities
   end 
 
+  def test_total_reunion_cost
+    reunion = Reunion.new("Denver")
+    hiking = Activity.new("Hiking", 30)
+    bowling = Activity.new("Bowling", 50)
+    assert_equal 0, reunion.total_cost
+    reunion.add_activity(hiking)
+    reunion.add_activity(bowling)
+    assert_equal 80, reunion.total_cost
+  end 
+
+  def test_debt_breakdown
+    reunion = Reunion.new("Denver")
+    hiking = Activity.new("Hiking", 30)
+    bowling = Activity.new("Bowling", 30)
+
+    hiking.add_participant("Jane", 5)
+    hiking.add_participant("Bob", 10)
+    hiking.add_participant("Sam", 13)
+
+    bowling.add_participant("Jane", 15)
+    bowling.add_participant("Bob", 5)
+    bowling.add_participant("Sam", 20)
+
+    reunion.add_activity(hiking)
+    reunion.add_activity(bowling)
+
+    breakdown = reunion.breakdown 
+    expected = {"Hiking" => {"Jane" => 5, "Bob" => 0, "Same" => -3}, "Bowling" => {"Jane" => -5, "Bob" => 5, "Sam" => -10}}
+    assert_equal expeceted, actual
+  end 
 end
