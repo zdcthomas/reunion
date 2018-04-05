64b82c73d2085ef52f90344e28e6d4dd0ce930fd
zdcthomas <zdcthomas@yahoo.com>
2018-04-05 16:48:24 -0600

    TESTS iteration 3 reunion location

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,15 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  def test_it_exists
+    reunion = Reunion.new("Denver")
+    assert_instance_of Reunion, reunion
+  end 
+
+  def test_reunion_has_location
+    reunion = Reunion.new("Denver")
+    assert_equal "Denver", reunion.location
+  end 
+  
 end
